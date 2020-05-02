b11655123714572f520597d91eed0c485256f667
Orane Ellis <57927175+ellisorane@users.noreply.github.com>
2020-05-01 22:28:27 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,37 +1,48 @@
-const tour = document.getElementById('tour');
-let floatingDiv;
+const tour = document.getElementById('tour')
+let backdrop
+let floatingDiv
+let floatingForm
 
 tour.addEventListener('click', (e) => {
-    e.preventDefault();
-    // Create floating div
+    e.preventDefault()
+    
     if(!floatingDiv) {
-        floatingDiv = document.createElement('div');
-        floatingDiv.setAttribute('id', 'floating-div');
-        floatingDiv.style.display = 'flex';
-        floatingDiv.style.top = `${window.pageYOffset + 90}px`;
-        const floatingForm = '<form action="" class="bg-light" id="floating-form"><div class="row"><div class="col"><button type="button" class="close" id="close-form" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div><div class="row"><div class="col"><input type="text" placeholder="First Name"></div><div class="col"><input type="text" placeholder="Last Name"></div></div><div class="row"><div class="col"><input type="text" placeholder="Email"></div><div class="col"><input type="text" placeholder="Phone"></div></div><div class="row"><div class="col"><label for="message">Message</label><br><textarea rows="3"></textarea> <br></div></div><input type="submit" value="Submit" id="submit-tour" class="btn btn-primary"></form>'
-        floatingDiv.innerHTML = floatingForm;
-        document.body.appendChild(floatingDiv);   
-    } 
+        //Create form container
+        floatingDiv = document.createElement('div')
+        floatingDiv.setAttribute('id', 'floating-div')
+        floatingDiv.style.display = 'block'
+
+        //Create form
+        floatingForm = document.createElement('form')
+        floatingForm.innerHTML = '<div class="row"><div class="col"><button type="button" class="close" id="close-form" aria-label="Close"><span aria-hidden="true">&times;</span></button></div></div><div class="row"><div class="col"><input type="text" placeholder="First Name"></div><div class="col"><input type="text" placeholder="Last Name"></div></div><div class="row"><div class="col"><input type="text" placeholder="Email"></div><div class="col"><input type="text" placeholder="Phone"></div></div><div class="row"><div class="col"><label for="message">Message</label><br><textarea rows="3"></textarea> <br></div></div><input type="submit" value="Submit" id="submit-tour" class="btn btn-primary">'
+        floatingForm.setAttribute('id', 'floating-form')
+        floatingForm.style.marginTop = `${window.pageYOffset + 100}px`
+
+        //Create backdrop
+        backdrop = document.createElement('div')
+        backdrop.setAttribute('class', 'backdrop')
+
+        //Add elements to UI
+        floatingDiv.appendChild(floatingForm)
+        document.body.insertAdjacentElement('afterbegin', floatingDiv)
+        document.body.insertAdjacentElement('afterbegin', backdrop)
+    } else {
+        floatingDiv.style.display = 'block'
+        backdrop.style.display = 'block'
+    }
 
     //close button
-    const closeForm = document.getElementById('close-form');
-    if(floatingDiv.style.display = 'flex'){
-        closeForm.addEventListener('click', removeFloatingDiv)
-    } 
-
-    //Remove floatingDiv function
-    function removeFloatingDiv() {
-        floatingDiv.style.display = 'none';
-    }
+    document.getElementById('close-form').addEventListener('click', () => {
+        floatingDiv.style.display = 'none'
+        backdrop.style.display = 'none'
+    })
 
 });
 
-// Keep form at the top of viewport 
-const floatForm = document.getElementById('floating-form');
+// Keep form at the top of viewport
 window.addEventListener('scroll', () => {
     if(floatingDiv) {
-        floatingDiv.style.top = `${window.pageYOffset + 90}px`;
+        floatingForm.style.marginTop = `${window.pageYOffset + 100}px`
     }
 });
 
